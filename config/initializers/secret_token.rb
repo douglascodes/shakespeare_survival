83ab438da32b58348441ef5b1dd547e22e1224c6
douglascodes <douglasking215@gmail.com>
2013-04-04 22:03:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shakes::Application.config.secret_token = '88ce7ed79b56c01a91de4cc9dca3cfac998692df6fabc7941978163a38cfac89573fcd55489ec2de3b5506a8a841571b673f9420f10d30b6a91343d3b5a6a65b'
